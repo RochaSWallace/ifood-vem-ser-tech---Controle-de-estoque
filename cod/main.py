cf6bb57806833ad5dc8b9fc438d02a4637c2d6ce
Wallace <wallacesouzarocha@gmail.com>
2023-11-12 15:10:36 -0300

    Criando função cadastro e chamando a mesma

--- cod/main.py
+++ cod/main.py
@@ -1,6 +1,27 @@
 
+def cadastro_produto() -> list:
+
+    print("bem vindo ao cadastro de produtos!")
+    produto = {}
+
+    produto["id"] = input("Digite o id do produto: ")
+    produto["nome"] = input("Digite o nome do produto a ser cadastrado: ")
+    produto["valor"] = float("Digite o valor do produto: ")
+    produto["quantidade"] = int(input("Digite a quantidade do produto em estoque: "))
+    while True:
+        especificacao = input("Digite o tipo de especificação que deseja adicionar ou S para sair: ").lower()
+        if especificacao == "s":
+            break
+        produto[especificacao]  = input("Digite o dado da especificação: ")
+    texto_descrivo = input("Caso deseje adicionar um texto descritivo, digite S: ").lower()
+    if texto_descrivo == "s":
+        produto["descricao"] = input("Digite o texto descritivo: ")
+    return produto
+
+
 def main():
 
+    produtos = []
     while True:
         print("""Menu principal: 
             1 - Cadastro produto
@@ -13,7 +34,7 @@ def main():
 
         match escolha:
             case 1:
-                pass
+                produtos.append(cadastro_produto())
             case 2:
                 pass
             case 3:
