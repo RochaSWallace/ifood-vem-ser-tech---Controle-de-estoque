daee664f1063190ceebc1e5255c5604c0776e261
Wallace <wallacesouzarocha@gmail.com>
2023-11-12 14:50:27 -0300

    Criando menu principal

--- cod/main.py
+++ cod/main.py
@@ -1,1 +1,26 @@
-pass
+
+def main():
+
+    while True:
+        print("""Menu principal: 
+            1 - Cadastro produto
+            2 - Consultar produto
+            3 - Listar produtos
+            4 - Atualizar produtos
+            5 - Excluir cadastro
+            6 - Sair""")
+        escolha = int(input("Escolha um das opções acima: "))
+
+        match escolha:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                break
